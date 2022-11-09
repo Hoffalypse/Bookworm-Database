c76439637c6dc36f1ca225bbc02f914151c9b61d
Bryan Wienhoff <bhoff1980@gmail.com>
2022-11-09 01:42:00 -0700

    working through resolver

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,30 +1,65 @@
-const { School, Class, Professor } = require('../models');
+const { AuthenticationError } = require("apollo-server-express");
+const { Book, User  } = require('../models');
+const { signToken } = require('../utils/auth');
 
 const resolvers = {
   Query: {
-    schools: async () => {
-      return await School.find({}).populate('classes').populate({
-        path: 'classes',
-        populate: 'professor',
-      });
-    },
-    classes: async () => {
-      return await Class.find({}).populate('professor');
-    },
-    class: async (parent, args) => {
-      return await Class.findById(args.id).populate('professor');
-    },
-    professors: async () => {
-      return await Professor.find({}).populate('classes');
+    me: async (parent, args, context, info ) => {
+      if (context.user) {
+        return await User.findOne({ _id: context.user._id });
+      }
+      throw new AuthenticationError('You need to be logged in!');
     },
   },
-  // Define the functions that will fulfill the mutations
+  
   Mutation: {
-    addSchool: async (parent, { name, location, studentCount }) => {
-      // Create and return the new School object
-      return await School.create({ name, location, studentCount });
+    login: async (parent, { email, password }) => {
+      const account = await User.findOne({ email });
+
+      if (!account) {
+        throw new AuthenticationError('No account with this email found!');
+      }
+
+      const correctPw = await account.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect password!');
+      }
+
+      const token = signToken(account);
+      return { token, account };
     },
-  },
+    addUser: async (parent, { username, email, password }) => {
+      const account = await Profile.create({ username, email, password });
+      const token = signToken(user);
+
+      return { token, account };
+    },
+
+    saveBook: async (parent, args, context, info ) => {
+      // If context has a `user` property, that means the user executing this mutation has a valid JWT and is logged in
+      if (context.user) {
+        return User.findOneAndUpdate(
+          { _id: context.user },
+          {
+            $addToSet: { savedBooks: args.input },
+          },
+          {
+            new: true,
+            runValidators: true,
+          }
+        );
+      }
+      // If user attempts to execute this mutation and isn't logged in, throw an error
+      throw new AuthenticationError('You need to be logged in!');
+    },
+    removeBook: async (parent, args, context) => {
+      if (context.user) {
+        return await Book.findOneAndDelete({savedBooks: { bookId: args.bookId }});
+      }
+      throw new AuthenticationError('You need to be logged in!');
+    },
+}
 };
 
 module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -20,7 +20,7 @@ const typeDefs = gql`
 
   type Auth {
     token: ID!
-    user: [User]
+    user: User
   }
   
   input savedBook {
@@ -38,7 +38,7 @@ const typeDefs = gql`
 
   
   type Mutation {
-    login(email: String!, password: String!) return Auth
+    login(email: String!, password: String!) : Auth
     addUser(username: String!, email: String!, password: String!): Auth
     saveBook(input: savedBook!): User
     removeBook(bookId: ID!): User
